cbeaac0e1b1d7b669d8fb7acd3634beb97080223
Mehadyhasanakash <mehadyhasan1000@gmail.com>
2022-10-25 20:10:58 +0600

    final

--- src/components/View.js
+++ src/components/View.js
@@ -2,9 +2,17 @@ import React from 'react'
 
 const View = ({datas}) => {
 
-  return datas.map ((data) =>{
+    return datas.map(data=>(
+        
+        <tr key={data.car}>
+            <td>{data.car}</td>
+            <td>{data.diver}</td>
+            <td>{data.Checkin}</td>
+            <td>{data.ChecOut}</td>
+                    
+        </tr>            
     
-  })
+))
 }
 
 export default View
